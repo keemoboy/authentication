1ba9b46b4f5de1991e62f35549ae130dd62d5cff
Apprentice <apprentice@devbootcamp.com>
2012-11-12 15:43:14 -0800

    done

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 gem 'devise'
 
+gem 'cancan'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize( user )
+    user ||= User.new
+
+    if user.is? :admin
+      can :manage, Article
+    else
+      can :read, Article
+    end
+  end
+
+end
